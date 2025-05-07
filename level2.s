fcc3e3f3730a5f09bca577a638ddfdd180e6ee91
VoNgocSinh <vongocsinh2007@gmail>
2025-05-07 09:03:59 +0000

    Create level2.s file

---
+++ level2.s
@@ -0,0 +1,17 @@
+.intel_syntax noprefix
+.globl _start
+
+.section .text
+
+_start:
+    mov rdi, 0x2    #AF_INET - Domain socket value
+    mov rsi, 0x1    #SOCK_STREAM - Type socket value
+    mov rdx, 0x0    #DEFAUT - Protocol socket value
+    mov rax, 0x29   #SYS_SOCKET - System socket value
+    syscall
+
+    xor rdi, rdi
+    mov rax, 60     #SYS_EXIT
+    syscall
+
+.section .data
